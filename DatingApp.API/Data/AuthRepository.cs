57723fe61781bc846aef5c40e3a7623a8664528b
Vishal Garg <visgarg@publicisgroupe.net>
2020-07-05 18:06:43 +0530

    End of Section 8

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -49,6 +49,7 @@ namespace DatingApp.API.Data
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
+            //new hmacsha512 creates a new instance while hmac.key generates random key
             using(var hmac=new System.Security.Cryptography.HMACSHA512()){
                 passwordSalt=hmac.Key;
                 passwordHash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
